e636b3e060587cf0a1cc1f9b7ac1b643884a8d5b
Dirk Moeller <dirk.moeller@systemengineers.de>
2017-09-12 21:25:45 +0200

    enable DEBUG_LEVELING_FEATURE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -887,6 +887,7 @@
  * NOTE: Requires a lot of PROGMEM!
  */
 //#define DEBUG_LEVELING_FEATURE
+#define DEBUG_LEVELING_FEATURE
 
 #if ENABLED(MESH_BED_LEVELING) || ENABLED(AUTO_BED_LEVELING_BILINEAR) || ENABLED(AUTO_BED_LEVELING_UBL)
   // Gradually reduce leveling correction until a set height is reached,
