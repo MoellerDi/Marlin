81f23b1684f101d19ec551dc6df7b6e1983ed842
Dirk Moeller <dirk.moeller@systemengineers.de>
2017-11-27 20:11:59 +0100

    tweak some speed settings: - DEFAULT_MAX_FEEDRATE - DEFAULT_MAX_ACCELERATION - DEFAULT_RETRACT_ACCELERATION - DEFAULT_TRAVEL_ACCELERATION

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -561,7 +561,8 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 15, 25 }
+//#define DEFAULT_MAX_FEEDRATE          { 500, 500, 15, 25 } //stock values
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -569,9 +570,10 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-//#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 } //default conf
+//#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 }  //default conf
 //#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 1000, 5000 } // TDH conf
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 } //stock values
+//#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 }  //stock values
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 25 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -582,8 +584,8 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000   // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_RETRACT_ACCELERATION  1000   // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   500    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
