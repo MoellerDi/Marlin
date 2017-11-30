2d4ac937017b5ccdec1eae6daa90e7aa95743e38
Dirk Moeller <dirk.moeller@systemengineers.de>
2017-11-30 19:45:03 +0100

    change GRID_MAX_POINTS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -936,7 +936,7 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 15
+  #define GRID_MAX_POINTS_X 13
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
