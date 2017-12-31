34117956491028844caa124c660fae5b124dc209
Dirk Moeller <dirk.moeller@systemengineers.de>
2017-12-31 15:08:46 +0100

    update config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -472,10 +472,10 @@
 // @section lcd
 
 // Include a page of printer information in the LCD Main Menu
-#define LCD_INFO_MENU
+//#define LCD_INFO_MENU
 
 // Leave out seldom-used LCD menu items to recover some Program Memory
-//#define SLIM_LCD_MENUS
+#define SLIM_LCD_MENUS
 
 // Scroll a longer status message into view
 #define STATUS_MESSAGE_SCROLLING
