7cfd80d213be8b5665e9b0efedfac9e6b3ce3d05
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-04-01 18:35:30 +0200

    TMC2130 - tuning config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1060,16 +1060,16 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          700  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
   #define X_CS_PIN            64  // HD overwrite X_CS_PIN
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT          700
   #define Y_MICROSTEPS        16
   #define Y_CS_PIN            44  // HD overwrite Y_CS_PIN
   #define E1_CS_PIN           -1  // HD overwrite E1_CS_PIN because it's needed for Y_CS_PIN
 
-  #define Z_CURRENT          650
+  #define Z_CURRENT          400
   #define Z_MICROSTEPS        16
 
   //#define X2_CURRENT         800
@@ -1081,7 +1081,7 @@
   //#define Z2_CURRENT         800
   //#define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         800
+  #define E0_CURRENT         400
   #define E0_MICROSTEPS       16
 
   //#define E1_CURRENT         800
