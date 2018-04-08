797e103b71ac0c6724cad4c4cc597beb8d04f2a7
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-04-08 17:59:47 +0200

    update config - some new speed related settings for testing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -569,7 +569,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
 //#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 25 } //stock values
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 15, 25 } // testing
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 25 } // testing
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -581,7 +581,7 @@
 //#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }    //stock values
 //#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 1000, 5000 } // TDH conf
 //#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 25 }    // my testing
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 300, 300, 10, 500 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -591,9 +591,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000   // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          50    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  50    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   100   // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -605,7 +605,7 @@
  */
 #define DEFAULT_XJERK                 10.0
 #define DEFAULT_YJERK                 10.0
-#define DEFAULT_ZJERK                  2.7
+#define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  5.0
 
 //===========================================================================
