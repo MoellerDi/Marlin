e63b902d1b973ecbe60f8174cc97e1ad2808e43d
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-05-01 12:25:53 +0200

    tuning speed for probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -731,7 +731,7 @@
 #define MIN_PROBE_EDGE 30
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 8000
+#define XY_PROBE_SPEED 4000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
@@ -1098,7 +1098,7 @@
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (30*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_Z  (3*60)
 
 // @section calibrate
 
