54e3474bca53f23cd06f71094e8405fb3e40499d
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-05-27 12:12:46 +0200

    TMC2130 - tuning config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1117,7 +1117,7 @@
   //#define Z2_CURRENT         800
   //#define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT        1100
+  #define E0_CURRENT         900
   #define E0_MICROSTEPS       16
 
   //#define E1_CURRENT         800
