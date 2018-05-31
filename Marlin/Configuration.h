81f9270a2a6e8f21d35056709c388602884ab6d5
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-05-31 08:12:03 +0200

    update config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -587,7 +587,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
 //#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 25 } //stock values
-#define DEFAULT_MAX_FEEDRATE          { 150, 150, 5, 50 } // testing
+#define DEFAULT_MAX_FEEDRATE          { 150, 150, 15, 50 } // testing
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -599,7 +599,7 @@
 //#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }    //stock values
 //#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 1000, 5000 } // TDH conf
 //#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 25 }    // my testing
-#define DEFAULT_MAX_ACCELERATION      { 500, 300, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -610,7 +610,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   1000   // X, Y, Z acceleration for travel (non printing) moves
 
 /**
@@ -621,9 +621,9 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
-#define DEFAULT_ZJERK                  0.3
+#define DEFAULT_XJERK                 20.0
+#define DEFAULT_YJERK                 20.0
+#define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  5.0
 
 /**
