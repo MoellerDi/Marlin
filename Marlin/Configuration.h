bced403d7ad3f2aff63953908b31c0023bff3c9d
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-08-24 20:29:14 +0200

    update config - some tuning for PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -664,7 +664,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION           600    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
+#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION    800    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
