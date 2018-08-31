e8879687befc3256bbf4df6040af1f8469b5270d
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-08-31 18:03:34 +0200

    new mount for e3D V6 clone - even more tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -382,9 +382,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   //e3D V6 Clone; PSU at 13V
-  #define DEFAULT_Kp 16.20
-  #define DEFAULT_Ki 1.34
-  #define DEFAULT_Kd 49.11
+  #define DEFAULT_Kp 19.06
+  #define DEFAULT_Ki 1.52
+  #define DEFAULT_Kd 59.72
 
   //CR-10 Stock Hotend; PSU at 13V (tuned with "M303 E0 S230 C15")
   //#define DEFAULT_Kp 15.44
