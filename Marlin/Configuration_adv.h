b04265aae7ebe37d5859dcc0a9ceecdf30199d8b
Dirk Moeller <dirk.moeller@systemengineers.de>
2018-11-29 20:23:49 +0100

    add some functionality

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -485,7 +485,7 @@
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
 //
-//#define JUNCTION_DEVIATION
+#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
   #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
 #endif
@@ -496,7 +496,7 @@
  * vibration and surface artifacts. The algorithm adapts to provide the best possible step smoothing at the
  * lowest stepping frequencies.
  */
-//#define ADAPTIVE_STEP_SMOOTHING
+#define ADAPTIVE_STEP_SMOOTHING
 
 /**
  * Custom Microstepping
